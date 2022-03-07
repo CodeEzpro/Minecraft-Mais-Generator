076c33845c41ccec3f22c73468fa208bf04d3b18
CodeEzpro <77250291+CodeEzpro@users.noreply.github.com>
2022-03-07 13:39:15 +0100

    Add files via upload

---
+++ mcmais.py
@@ -0,0 +1,13 @@
+import random
+
+actionlist = ["ouvrir mon inventaire","casser une fleur","récuperer un item","tuer un monstre","casser un bloc","tondre un mouton","frapper un mob","marcher","placer des blocs","l'XP","couper du bois","changer de monde","changer de case d'inventaire","cuire un item","mettre une armure","changer de biome","sauter","s'accoupir","chaque mob","manger","dormir"]
+conslist = "provoque une explosion","est OP",",inflige des dégàts","donne un coeur supplémentaire","donne un effet aléatoire","augmente ma taille","réduit ma taille","multiplie mes items","est impossible","augmente ma vitesse","augmente la hauteur de mes sauts","est cheat","donne un enchantement aléatoire","augmente mes dégàts","change mon pack de texture","me fait changer de dimension","inverse la gravité";
+
+action = random.choice(actionlist)
+cons = random.choice(conslist)
+
+print("Minecraft Mais "+action+" "+cons)
+
+
+#Retirer les cedits est Formelement interdit
+
